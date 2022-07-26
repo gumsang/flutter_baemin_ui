041a6135ec2f01fb0b9efae533dffa718bd473dc
eunsang.kim <gomuzom@naver.com>
2022-07-26 15:29:53 +0900

    feat CarouseSlider 사용, 카드부분을 클래스를 사용하여 바깥으로 빼줌

--- lib/baemin_main.dart
+++ lib/baemin_main.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+final dummyItems = [
+  'assets/dummy1.jpeg',
+  'assets/dummy2.jpeg',
+  'assets/dummy3.jpeg',
+  'assets/dummy4.jpeg',
+  'assets/dummy5.jpeg'
+];
 
 class BaeminMain extends StatefulWidget {
   const BaeminMain({Key? key}) : super(key: key);
@@ -65,185 +74,7 @@ class _BaeminMainState extends State<BaeminMain> {
                   )
                 ],
               ),
-              Row(
-                children: [
-                  Flexible(
-                    child: Card(
-                      color: const Color.fromARGB(0, 0, 0, 0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      elevation: 1,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                        ),
-                        child: Image.asset(
-                          "assets/baemin1.png",
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Flexible(
-                    child: Card(
-                      color: const Color.fromARGB(0, 0, 0, 0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      elevation: 1,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                        ),
-                        child: Image.asset(
-                          "assets/baedal.png",
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Card(
-                color: const Color.fromARGB(0, 0, 0, 0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                elevation: 1,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: const Color.fromARGB(255, 255, 255, 255),
-                  ),
-                  child: Image.asset(
-                    "assets/pojang.png",
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ),
-              Row(
-                children: [
-                  Flexible(
-                    child: Card(
-                      color: const Color.fromARGB(0, 0, 0, 0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      elevation: 1,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                        ),
-                        child: Image.asset(
-                          "assets/bmart.png",
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Flexible(
-                    child: Card(
-                      color: const Color.fromARGB(0, 0, 0, 0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      elevation: 1,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                        ),
-                        child: Image.asset(
-                          "assets/milkit.png",
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Card(
-                color: const Color.fromARGB(0, 0, 0, 0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                elevation: 1,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: const Color.fromARGB(255, 255, 255, 255),
-                  ),
-                  child: Image.asset(
-                    "assets/baeminstore.png",
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ),
-              Row(
-                children: [
-                  Flexible(
-                    child: Card(
-                      color: const Color.fromARGB(0, 0, 0, 0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      elevation: 1,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                        ),
-                        child: Image.asset(
-                          "assets/shoppinglive.png",
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Flexible(
-                    child: Card(
-                      color: const Color.fromARGB(0, 0, 0, 0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      elevation: 1,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                        ),
-                        child: Image.asset(
-                          "assets/gift.png",
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Flexible(
-                    child: Card(
-                      color: const Color.fromARGB(0, 0, 0, 0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      elevation: 1,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                        ),
-                        child: Image.asset(
-                          "assets/byulme.png",
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              const UiBody(),
             ],
           ),
         ),
@@ -264,6 +95,228 @@ class BackgroudTextField extends StatelessWidget {
             bottomLeft: Radius.circular(20.0),
             bottomRight: Radius.circular(20.0)),
         color: Color.fromARGB(255, 92, 184, 182),
+      ),
+    );
+  }
+}
+
+class UiBody extends StatelessWidget {
+  const UiBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Flexible(
+                child: Card(
+                  color: const Color.fromARGB(0, 0, 0, 0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  elevation: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                    ),
+                    child: Image.asset(
+                      "assets/baemin1.png",
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+              Flexible(
+                child: Card(
+                  color: const Color.fromARGB(0, 0, 0, 0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  elevation: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                    ),
+                    child: Image.asset(
+                      "assets/baedal.png",
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Card(
+            color: const Color.fromARGB(0, 0, 0, 0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            elevation: 1,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: const Color.fromARGB(255, 255, 255, 255),
+              ),
+              child: Image.asset(
+                "assets/pojang.png",
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Flexible(
+                child: Card(
+                  color: const Color.fromARGB(0, 0, 0, 0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  elevation: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                    ),
+                    child: Image.asset(
+                      "assets/bmart.png",
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+              Flexible(
+                child: Card(
+                  color: const Color.fromARGB(0, 0, 0, 0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  elevation: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                    ),
+                    child: Image.asset(
+                      "assets/milkit.png",
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Card(
+            color: const Color.fromARGB(0, 0, 0, 0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            elevation: 1,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: const Color.fromARGB(255, 255, 255, 255),
+              ),
+              child: Image.asset(
+                "assets/baeminstore.png",
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Flexible(
+                child: Card(
+                  color: const Color.fromARGB(0, 0, 0, 0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  elevation: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                    ),
+                    child: Image.asset(
+                      "assets/shoppinglive.png",
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+              Flexible(
+                child: Card(
+                  color: const Color.fromARGB(0, 0, 0, 0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  elevation: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                    ),
+                    child: Image.asset(
+                      "assets/gift.png",
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+              Flexible(
+                child: Card(
+                  color: const Color.fromARGB(0, 0, 0, 0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  elevation: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                    ),
+                    child: Image.asset(
+                      "assets/byulme.png",
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 90.0,
+              autoPlay: true,
+            ),
+            items: dummyItems.map((dummy) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Card(
+                    color: const Color.fromARGB(0, 0, 0, 0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    elevation: 1,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Image.asset(
+                        dummy,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  );
+                },
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
